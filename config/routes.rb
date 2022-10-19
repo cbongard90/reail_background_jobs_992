870c50f4153477088d6003eda916b39dca71cb16
Christian Bongard <christian.bongard90@gmail.com>
2022-10-19 09:06:18 +0400

    added sidekiq and fake job for background

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  # Sidekiq Web UI, only for admins.
+  require "sidekiq/web"
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
